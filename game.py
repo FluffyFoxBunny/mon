ba39a0a7dcf9ab9f7116de9e9ba963851025131c
Foxxo-Win <Foxxo@FOXXO-PC>
2021-02-12 23:42:25 +0000

    formatting

--- game.py
+++ game.py
@@ -4,10 +4,9 @@ import pygame
 
 class Game:
 
-    def __init__(self, scalex=32,scaley=32,timescale=1):
+    def __init__(self, scalex=32, scaley=32, timescale=1):
 
-        
-        timescale=0.05
+        timescale = 0.05
         mon.clear()
         myMon = mon.Mon()
 
@@ -15,64 +14,46 @@ class Game:
         scaley = 24
 
         pygame.init()
-        gamewin = pygame.display.set_mode((32*scalex,32*scaley))
+        gamewin = pygame.display.set_mode((32*scalex, 32*scaley))
         pygame.display.set_caption("mon")
 
-        
-        BG_COLOUR = (255,255,255)
-        
+        BG_COLOUR = (255, 255, 255)
+        BG_COLOUR2 = (240, 240, 240)
 
         mainClock = pygame.time.Clock()
-
-
-
-        debugfont = pygame.font.Font(pygame.font.get_default_font(),16)
+        debugfont = pygame.font.Font(pygame.font.get_default_font(), 16)
 
         statuses = myMon.splitStatus()
 
         stexts = []
         srects = []
 
-        
+        for i in range(len(statuses)):
 
-        for i in range(len(statuses)):  
-            
-
-            thisone = debugfont.render(statuses[i],True,(0,0,0))    
+            thisone = debugfont.render(statuses[i], True, (0, 0, 0))
             stexts.append(thisone)
             thisonesrect = thisone.get_rect()
             thisonesrect.x = 16
             thisonesrect.y = 16*(i+1)
             srects.append(thisonesrect)
-        
-      
-               
 
-      
-            
-
-        
-        
-        
-    
         gameon = True
 
         timedoober = 0
         secs = 0
-        
-        frame=0
-        
-        
+
+        frame = 0
+
         while gameon:
 
             mainClock.tick(60)
 
             gamewin.fill(BG_COLOUR)
 
-            BG_COLOUR2 = (240, 240, 240)
-            playarearect = pygame.rect.Rect(0*scalex,8*scaley,32*scalex,16*scalex)
+            playarearect = pygame.rect.Rect(
+                0*scalex, 8*scaley, 32*scalex, 16*scalex)
 
-            gamewin.fill(rect=playarearect,color=BG_COLOUR2)
+            gamewin.fill(rect=playarearect, color=BG_COLOUR2)
 
             if myMon.alive:
                 if frame == 0:
@@ -82,45 +63,37 @@ class Game:
 
             else:
                 montouse = pygame.image.load("img/grave.png")
-            bigspr= pygame.transform.scale(montouse,(16*scalex,16*scaley))
-            gamewin.blit(bigspr,(8*scalex,8*scaley))
-
-
+            bigspr = pygame.transform.scale(montouse, (16*scalex, 16*scaley))
+            gamewin.blit(bigspr, (8*scalex, 8*scaley))
 
             statuses = myMon.splitStatus()
 
-
-
-            rn = 0
-
-            
             for i in range(len(stexts)):
 
-
-                stexts[i]=debugfont.render(statuses[i], True,(0,0,0))
-                gamewin.blit(stexts[i],srects[i])
+                stexts[i] = debugfont.render(statuses[i], True, (0, 0, 0))
+                gamewin.blit(stexts[i], srects[i])
 
             for event in pygame.event.get():
-                if event.type==pygame.QUIT:
-                    gameon=False
-            
-            timedoober+=1
-            if timedoober>=60:
-                frame+=1
-                timedoober=0
-                secs+=1
-            if secs>=timescale:
-                secs=0
+                if event.type == pygame.QUIT:
+                    gameon = False
+
+            timedoober += 1
+            if timedoober >= 60:
+                frame += 1
+                timedoober = 0
+                secs += 1
+            if secs >= timescale:
+                secs = 0
                 if myMon.alive:
                     myMon.update()
 
             if frame > 1:
                 frame = 0
 
-            
             pygame.display.flip()
-            
+
         pygame.quit()
+
 
 if __name__ == "__main__":
 
